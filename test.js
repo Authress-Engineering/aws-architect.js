5283be27f133cedd68af23b7b9286534b609693f
Warren Parad <wparad@gmail.com>
2015-10-22 05:12:43 -0400

    Add in travis-build-tools.

---
+++ test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var mocha = require('mocha');
+var assert = require('chai').assert;
+
+var index = require('./lib/index.js');
+
+describe('index', function() {
+	describe('#Version()', function () {
+		it('Should exist', function(){
+			//assert.fail('failure');
+		});
+	});
+});
