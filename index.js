d4f74688ab72ffd62ccc20a6d3b16d672bf77599
Warren Parad <wparad@gmail.com>
2021-12-09 13:08:26 +0100

    Parse JSON safely to handle yaml templates in short curcuit. fix #52

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-/*
-	Automatically configure microservice in AWS
-	Copyright (C) 2018 Warren Parad
-
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program.	If not, see <https://www.gnu.org/licenses/>.
-*/
-
 let archiver = require('archiver');
 let aws = require('aws-sdk');
 let exec = require('child_process').exec;
